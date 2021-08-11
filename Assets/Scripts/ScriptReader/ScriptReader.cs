ee083e641b92e9edd91abc99945bb223a45e4090
TianC Wang <thewater2016@qq.com>
2021-08-11 16:01:14 +0800

    Script Reader Fix#2

--- Assets/Scripts/ScriptReader/ScriptReader.cs
+++ Assets/Scripts/ScriptReader/ScriptReader.cs
@@ -11,14 +11,15 @@ namespace ScriptReader
     /// <summary>Plot script input.</summary>
     class ScriptReader : ITextParser_Mk004
     {
+        private Queue<Unit_Mk004> _Units = new Queue<Unit_Mk004>(0);
         private Stack<Unit_Mk004> Units = new Stack<Unit_Mk004>(0);
         private void CrackString(string scriptString)
         {
             int endIndex;
             while ((endIndex = scriptString.IndexOf("//")) != -1)
                 scriptString = scriptString.Remove(endIndex, scriptString.IndexOf('\n', endIndex + 1) - endIndex - 1);
-            Stack<string> contents = new Stack<string>(0);
-            Stack<int> indexs = new Stack<int>(0);
+            Queue<string> contents = new Queue<string>(0);
+            Queue<int> indexs = new Queue<int>(0);
             for (int i = 0; i < scriptString.Length; i++)
             {
                 switch (scriptString[i])
@@ -26,13 +27,16 @@ namespace ScriptReader
                     // ${index}$
                     case '$':
                         endIndex = scriptString.IndexOf('$', i + 1);
+                        if (Units.Count > 0)
+                        {
+                            Units.Peek().Contents = contents.ToArray();
+                            Units.Peek().PossibleNextIndexs = indexs.ToArray();
+                        }
                         Units.Push(new Unit_Mk004());
                         Units.Peek().SelfIndex = int.Parse(scriptString.Substring(i + 1, endIndex - i - 1));
                         Units.Peek().Style = TextStyle.EventTrigger;
-                        Units.Peek().Contents = contents.ToArray();
-                        Units.Peek().PossibleNextIndexs = indexs.ToArray();
-                        contents = new Stack<string>(0);
-                        indexs = new Stack<int>(0);
+                        contents = new Queue<string>(0);
+                        indexs = new Queue<int>(0);
                         i = endIndex + 1;
                         break;
                     // [{speaker}]
@@ -44,7 +48,7 @@ namespace ScriptReader
                     // :plaintext
                     case ':':
                         endIndex = scriptString.IndexOf("\r\n", i + 1);
-                        contents.Push(scriptString.Substring(i + 1, endIndex - i - 1));
+                        contents.Enqueue(scriptString.Substring(i + 1, endIndex - i - 1));
                         Units.Peek().Style = TextStyle.Plain;
                         i = endIndex + 1;
                         break;
@@ -54,16 +58,16 @@ namespace ScriptReader
                     case '#':
                         endIndex = scriptString.IndexOf('#', i + 1);
                         if (Units.Peek().Style == TextStyle.Plain)
-                            indexs.Push(int.Parse(scriptString.Substring(i + 1, endIndex - i - 1)));
+                            indexs.Enqueue(int.Parse(scriptString.Substring(i + 1, endIndex - i - 1)));
                         else
                         {
-                            contents.Push(scriptString.Substring(i + 1, endIndex - i - 1));
+                            contents.Enqueue(scriptString.Substring(i + 1, endIndex - i - 1));
                             if (scriptString.IndexOf('#', endIndex + 1) < scriptString.IndexOf('\n', endIndex + 1))
                             {
                                 Units.Peek().Style = TextStyle.Selective;
                                 i = endIndex;
                                 endIndex = scriptString.IndexOf('#', endIndex + 1);
-                                indexs.Push(int.Parse(scriptString.Substring(i + 1, endIndex - i - 1)));
+                                indexs.Enqueue(int.Parse(scriptString.Substring(i + 1, endIndex - i - 1)));
                             }
                         }
                         i = endIndex + 1;
@@ -71,6 +75,15 @@ namespace ScriptReader
                     default:
                         break;
                 }
+            }
+            int size = Units.Count;
+            for(int i = 0; i < size; i++)
+            {
+                _Units.Enqueue(Units.Pop());
+            }
+            for (int i = 0; i < size; i++)
+            {
+                Units.Push(_Units.Dequeue());
             }
         }
         public bool SetTargetFile(string scriptFilePath)
